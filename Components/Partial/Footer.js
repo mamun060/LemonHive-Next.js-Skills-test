236de075ed3715b29a40bfc3c88572954ebfc012
mamun060 <obujraju420@gmail.com>
2022-08-13 15:45:45 +0600

    ui setup with tailwind done

---
+++ Components/Partial/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=' bg-gray-900 text-white text-center h-10 flex items-center justify-center'> Our Footer Section </div>
+  )
+}
+
+export default Footer
